9cd222fb26f6c28d7a836328b8117f471395d8da
natekreit <nathan.kreit@mymail.champlain.edu>
2025-02-16 11:27:30 -0500

    add a ssh file

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -4,7 +4,7 @@
 #creates a new ssh user using $1 parameter
 #adds a public key from the local repo or curled from the remote repo
 #removes roots ability to ssh in
-echo "
+
 #new user
 if [ -z "$1" ]; then
   echo "Error: No username provided."
